54d580402582df876c54af102da3c7b08e7f29ff
Soumyadip Chakrabarti <soumyadipom7112005@gmail.com>
2026-09-02 14:20:53 +0530

    Remove iOS, macOS, Linux, Web, and Windows platform folders; remove unused desktop SQLite FFI dependency

--- night_watch_flutter/lib/services/database_service.dart
+++ night_watch_flutter/lib/services/database_service.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
-import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:sqflite/sqflite.dart';
 
 import 'package:night_watch_flutter/models/event_model.dart';
 import 'package:night_watch_flutter/models/session.dart';
@@ -22,11 +22,6 @@ class DatabaseService {
   }
 
   Future<Database> _initDatabase() async {
-    if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
-      sqfliteFfiInit();
-      databaseFactory = databaseFactoryFfi;
-    }
-
     final docsDir = await getApplicationDocumentsDirectory();
     final dbPath = p.join(docsDir.path, 'night_watch.db');
 
